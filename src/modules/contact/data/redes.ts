35d0d866845a00ccacbeb0f9c1538b7b15b634d9
alexander <alexander_pere@hotmail.com>
2025-10-17 20:07:54 -0500

    redes sociales

--- src/modules/contact/data/redes.ts
+++ src/modules/contact/data/redes.ts
@@ -4,20 +4,24 @@ import igIcon from '@public/redes/ig.webp';
 import ytIcon from '@public/redes/yt.webp';
 
 export const socialLinks = [
-  {
-    name: 'Facebook',
-    url: 'https://www.facebook.com/thedooragency',
-    icon: fbIcon,
-  },
-  {
-    name: 'Whatsapp',
-    url: 'https://wa.me/51977435348?text=Hola%2C%20estoy%20interesado%20en%20conocer%20más%20sobre%20los%20servicios%20de%20su%20agencia.',
-    icon: wspIcon,
-  },
-  {
-    name: 'Instagram',
-    url: 'https://www.instagram.com/thedoor.agency/',
-    icon: igIcon,
-  },
-  { name: 'Youtube', url: 'https://www.youtube.com/@TheDoorAgency', icon: ytIcon },
+	{
+		name: 'Facebook',
+		url: '#',
+		icon: fbIcon,
+	},
+	{
+		name: 'Whatsapp',
+		url: 'https://wa.me/51907466257?text=Hola%2C%20estoy%20interesado%20en%20conocer%20sus%20servicios.',
+		icon: wspIcon,
+	},
+	{
+		name: '#',
+		url: 'https://www.instagram.com/thedoor.agency/',
+		icon: igIcon,
+	},
+	{
+		name: '#',
+		url: 'https://www.youtube.com/@TheDoorAgency',
+		icon: ytIcon,
+	},
 ];
